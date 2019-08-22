3d329c78bb3183afdff6856620884be88a36a7ba
Raflisb <raflisetiawan.b@gmail.com>
2019-08-22 22:30:34 +0700

    tes

--- app/Http/Controllers/FooController.php
+++ app/Http/Controllers/FooController.php
@@ -9,7 +9,7 @@ class FooController extends Controller
     // menampilkan semua output dari method di atas dalam bentuk list
 
     public function callAllMethod() { 
-        echo Foo::baz1(); 
-        echo Foo::baz2();
+        echo Foo::baz2(); 
+        
     }
 }
--- app/MyClass/Foo.php
+++ app/MyClass/Foo.php
@@ -6,11 +6,26 @@ use Illuminate\Support\Facades\Facade;
 
 Class Foo { 
 
-    public function baz1() { 
-        return 'baz1'; 
+    public function baz1($string) { 
+      
+        $reverse = '';
+        $i = 0;
+        while(!empty($string[$i])){ 
+              $reverse = $string[$i].$reverse; 
+              $i++;
+        }
+        return $reverse;
     }
     public function baz2() { 
-        return 'baz2';
+        
+        for ($i = 0 ; $i<=15; $i++){
+            if ($i%3==0){
+                echo 'iorem';
+            }
+            else{
+            echo $i.", ";
+            }
+        }
     }
 }
 
